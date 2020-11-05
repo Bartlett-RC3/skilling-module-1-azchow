c022f16f6803c1e04dac969b5c93c023970acbb8
azchow <73367916+azchow@users.noreply.github.com>
2020-11-05 18:41:55 +0000

    Add files via upload

---
+++ Assets/Scripts/Homework/Session2HomeWork.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Deployment.Internal;
+using System.Diagnostics;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    
+    //Colour Names Array
+    string[] colours = {"black", "white","blue","yellow","purple"}
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(colours[1]);
+        //LOOPS
+        //For loop(counter,end condition,step)
+        for (int counter = 0; Counter < colours.Length; counter = counter + 1) ;
+         {
+            Debug.Log("The current colour at pos " + counter);
+            Debug.Log{colours[counter]};
+
+         }
+        int sumOfOddNumbers = 0;
+        for (int i = 1; i < 100; i = i + 2) ;
+        {
+          sumOfOddNumbers+=i;
+            Debug.Log("All odd numbers to 100 add to");
+        }
+        //shorthand for loop
+        foreach (string currentColour in colours) ;
+        {
+            Debug.Log("I want to buy an apple " + currentColour);
+
+        }
+        //while loop(need a condition to end otherwise may break my computer)
+        int whileCounter = 0;
+        string apple;
+        while (whileCounter < colours.Length) ;
+        {
+            apple += colours[whileCounter]+",";
+            whileCounter+=2;
+        }
+        Debug.Log(apple);
+        //CINDITIONALS(asking the computer questions)
+        //If satement
+        for (int i = 0; i =< 200; i++) ;
+        {
+            if (i % 2 == 0) ;
+            {
+                Debug.Log("Whole number:" + i);
+            }
+            else
+            {
+                Debug.Log("Odd mumber" + i);
+            }
+            //Asking multiple questions
+            bool question1Answer = true;
+            bool question2Answer = true;
+            bool question3Answer =false;
+            //Two conditions met(Concatenation)
+            if (question1Answer && question2Answer) ;
+            {
+                Debug.Log("Both answers are ture");
+            }
+            //Or question(one condition is met form two)
+            if (question1Answer || question3Answer)
+            {
+                Debug.Log("One of the answer is true");
+            }
+            //More than two questions
+            if (question1Answer || question2Answer||question3Answer)
+            {
+               //Action here
+                Debug.Log("One of the answer is true");
+            }
+            //question inside of question
+            if (question1Answer==true)
+            {
+                if (question2Answer == true)
+                {
+                    if (question3Answer == false)
+                }
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Homework/Session3Homework_yuehong.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //variables
+    public GameObject cube;
+    public int numberOfCubes=10;
+    public float spacing = 2.0f;
+    private List<GameObject> cubeCopies = new List<GameObject>();
+    private int frameCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //create an array of cubes sapced apart
+        for (int i = 0; i < numberOfCubes; i++)
+        {
+            Vector3 cubeCopyPosition = new Vector3(i * spacing, 1, 1);
+            Quaternion cubeCopyRotation = Quaternion.identity;
+            GameObject cubeCopy = Instantiate(cube, cubeCopyPosition, cubeCopyRotation);
+            Color cubeCopyColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            cubeCopy.GetComponent<MeshRenderer>().material.color = cubeCopyColor;
+            cubeCopies.Add(cubeCopy);
+                
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < cubeCopies.Count; i++)
+        {
+            GameObject cubeCopy = cubeCopies[i];
+            Color cubeCopyColor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            cubeCopy.GetComponent<MeshRenderer>().material.color = cubeCopyColor;
+
+        }
+
+        //Time since game started
+        Debug.Log("Frames since game started:" + frameCount);
+        frameCount++;
+
+        Debug.Log("Unity counted frames:" + Time.frameCount);
+        Debug.Log("Unity draws a frame in:" + Time.deltaTime + "seconds");
+
+        //Moving things in Unity 
+        foreach (GameObject cubeCopy in cubeCopies)
+        {
+            //Translate
+            Vector3 moveCopy = new Vector3(0, Random.Range(-1.5f, 1.5f), 0);
+            cubeCopy.GetComponent<Transform>().Translate(moveCopy * Time.deltaTime);
+            //Rotate
+            Vector3 rotateCopy = new Vector3(0, Random.Range(-90.0f, 90.0f), 0);
+            cubeCopy.transform.Rotate(rotateCopy);
+            //Scale
+            float scaleAmount = Random.Range(0.5f, 2.0f);
+            Vector3 scaleCopy = new Vector3(scaleAmount, scaleAmount, scaleAmount);
+            cubeCopy.transform.localScale = scaleCopy;
+
+
+        }
+        //Input
+        //Keyboard Input
+        if (Input.GetKey(KeyCode.Space))
+        {
+            foreach(GameObject cubeCopy in cubeCopies)
+            {
+                float scaleAmount = 2.0f;
+                Vector3 scaleCopy = new Vector3(scaleAmount, scaleAmount, scaleAmount);
+                cubeCopy.transform.localScale = scaleCopy;
+            }
+        }
+
+        //Mouse Input
+        if (Input.GetMouseButton(0)) 
+        {
+            foreach (GameObject cubeCopy in cubeCopies)
+            {
+                Color redcolor = new Color(1, 0, 0);
+                cubeCopy.GetComponent<MeshRenderer>().material.color = redcolor;
+            }
+        }
+
+    }
+}
