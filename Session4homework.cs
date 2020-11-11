5c9a6dee23368ccfded6483ce49dd56ad7fcffa3
azchow <73367916+azchow@users.noreply.github.com>
2020-11-11 15:52:10 +0000

    Add files via upload

---
+++ Session4homework.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session4homework : MonoBehaviour
+{
+    //Variables
+    public GameObject columnPrefab;
+    IEnumerator createPrefabsCoroutine;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createPrefabsCoroutine = DropPrefabs();
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calling (using) a coroutine
+        StartCoroutine(createPrefabsCoroutine);
+        Debug.Log(Time.time);
+        if(Time.time>10)
+        {
+            //stop a specific coroutine
+            StopCoroutine(createPrefabsCoroutine);
+            //stop all coroutines
+            StopAllCoroutines();
+        }
+    }
+
+    //Coroutines
+    IEnumerator DropPrefabs()
+    {
+        while(true)
+        {
+            //Action the coroutine(create and drop column)
+            Vector3 dropPosition = new Vector3(Random.RandomRange(-5.0f, 12.0f), Random.RandomRange(3.0f, 14.0f), Random.RandomRange(-7.0f, 6.0f));
+            Quaternion dropRotaion = new Quaternion(Random.RandomRange(0, 90), Random.RandomRange(0, 90), Random.RandomRange(0, 90), 1);
+            Instantiate(columnPrefab, dropPosition, dropRotaion);
+            yield return new WaitForSeconds(5);
+        } 
+
+    }
+
+}
---
+++ Session4homeworkpart2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Session4homeworkpart2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Check if an obiect is seeing another obiect using Ray Casting
+        //Get the position from where we are casting the ray
+        Vector3 rayCastPosition = this.gameObject.transform.position;
+        //Create a ray in the direction we want to cast
+        Vector3 rayCastDrection = this.gameObject.transform.TransformDirection(Vector3.forward);
+       //creat a varible that stores the information about what we are hitting
+        RaycastHit objectSeenByRay;
+        //Cast the ray
+        if (Physics.Raycast(rayCastPosition, rayCastDrection, out objectSeenByRay))
+        {
+            Debug.Log("Obeject seen:" + objectSeenByRay.transform.name);
+            Debug.Log("Obeject position:" + objectSeenByRay.transform.ToString());
+            Debug.Log("Point where ray is hitting the object:" + objectSeenByRay.point);
+            Destroy(objectSeenByRay.transform.gameObject);
+
+        }
+
+       
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move object up
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.gameObject.transform.Translate(Vector3.up * 0.1f);
+        }
+        //Move object down
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.gameObject.transform.Translate(Vector3.down * 0.1f);
+        }
+
+    }
+}
